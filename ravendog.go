fd5107a20c6d19b18cb291938924b3d4c5fe0fcc
George Thomas <gcthomas@uw.edu>
2025-06-21 20:52:41 -0700

    Use brace not parent for func

--- ravendog.go
+++ ravendog.go
@@ -1,12 +1,8 @@
 // Package ravendogutils has utility fucntions for dogs.
 package ravendogutils
 
-import (
-	"fmt"
-)
-
 // Years calculates dog years based on human years input.
 // Output is an int.
-func Years(humanYears int) int (
+func Years(humanYears int) int {
 	return humanYears * 7
-)
+}
